54323728691697a27a5c6a221002d14fe64a41fe
Richard Kim <ric_6696@Richards-MacBook-Pro.local>
2025-07-01 15:20:51 +0900

    Completed Neetcode Medium difficulty problem

--- Algorithms/GroupAnagrams.py
+++ Algorithms/GroupAnagrams.py
@@ -1,7 +1,17 @@
 import sys
+from typing import DefaultDict
 
 
 def groupAnagrams(strs):
+    res = DefaultDict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+        res[tuple(count)].append(s)
+
+    return list(res.values())
+
 
 def main():
     strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
